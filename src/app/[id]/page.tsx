6b82c69f53971a6faa317c1b3df5ad1b1cd430fb
Aadi Umrani <aadi.p.um@gmail.com>
2024-08-18 19:32:43 -0400

    Added the other user display feature

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -5,15 +5,33 @@ import Header from "@/app/widgets/header";
 import {FaEdit} from "react-icons/fa";
 import FlipCard from "@/app/widgets/flashcard";
 
+interface Author {
+    name: string;
+    email: string;
+}
+
 export default function FlashSetPage() {
     const router = useRouter();
     const {id} = useParams();
     const [userId, setUserId] = useState("");
+    const [author, setAuthor] = useState<Author>({name: "", email: ""});
     const [flashCardUserId, setFlashCardUserId] = useState("");
     const [flashCards, setFlashCards] = useState([]);
     const [flashSetName, setFlashSetName] = useState("");
 
     useEffect(() => {
+        const getAuthor = async (userId: any) => {
+            const res = await fetch(`/api/user/${userId}`, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem("token")}`
+                }
+            });
+            if (res.ok) {
+                const data = await res.json();
+                console.log(data);
+                setAuthor(data);
+            }
+        }
         const fetchFlashCards = async (userId: String) => {
             const res = await fetch(`/api/flash/get/set/${id}`, {
                 headers: {
@@ -23,6 +41,7 @@ export default function FlashSetPage() {
             const data = await res.json();
             setFlashCards(data.flashSet.set);
             setFlashCardUserId(data.flashSet.userId);
+            await getAuthor(data.flashSet.userId);
             setFlashSetName(data.flashSet.name);
         };
         const checkAuth = async () => {
@@ -45,27 +64,42 @@ export default function FlashSetPage() {
         checkAuth();
     }, [id, router]);
 
+
     return (
         <div className="min-h-screen flex flex-col bg-gray-900 text-white">
             <Header />
             <main className="flex-grow p-4">
                 <button onClick={() => router.back()} className="mb-4 bg-gray-800 p-2 rounded">Back</button>
-                <div className="flex flex-row mb-4 w-full justify-center items-center">
-                    <h2 className="text-4xl mr-4 text-center">{flashSetName}</h2>
-                    {
-                        userId === flashCardUserId && (
-                            <button className="bg-gray-800 p-2 rounded mb-4" onClick={
-                                () => router.push(`/${id}/edit`)
-                            }><FaEdit/></button>
-                        )
-                    }
+                <div className="flex flex-col mb-4 w-full justify-center items-center">
+                    <div className="flex flex-row mb-4 w-full justify-center items-center">
+                        {
+                            userId === flashCardUserId && (
+                                <button className="bg-gray-800 p-2 rounded mb-4" onClick={
+                                    () => router.push(`/${id}/edit`)
+                                }><FaEdit/></button>
+                            )
+                        }
+                        <h2 className="text-4xl mr-4 text-center">{flashSetName}</h2>
+                    </div>
+                    <h3 className="text-xl text-gray-400">
+                        {
+                            (userId !== flashCardUserId) ? (
+                                <button onClick={() => router.push(`/user/${flashCardUserId}`)} className="hover:underline">
+                                    {author.name}
+                                </button>
+                            ) : (
+                                author.name
+                            )
+                        }
+                    </h3>
                 </div>
-                <div className="flex flex-col gap-4">
-                    {flashCards.map((flashCard: any) => (
-                        <FlipCard key={flashCard._id} id={flashCard._id} question={flashCard.question} answer={flashCard.answer} />
-                    ))}
-                </div>
+                    <div className="flex flex-col gap-4">
+                        {flashCards.map((flashCard: any) => (
+                            <FlipCard key={flashCard._id} id={flashCard._id} question={flashCard.question}
+                                      answer={flashCard.answer}/>
+                        ))}
+                    </div>
             </main>
         </div>
-    );
+);
 }
---
+++ src/app/user/[id]/page.tsx
@@ -0,0 +1,71 @@
+"use client";
+import { useEffect, useState } from "react";
+import {useParams, useRouter} from "next/navigation";
+import Header from "@/app/widgets/header";
+
+interface User {
+    name: string;
+    email: string;
+}
+
+export default function OtherUserPage() {
+    const [user, setUser] = useState<User | null>(null);
+    const {id} = useParams();
+    const [loading, setLoading] = useState(true); // Initial loading state
+    const router = useRouter();
+
+    useEffect(() => {
+        const fetchUserData = async () => {
+            const res = await fetch(`/api/user/${id}`, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem("token")}`
+                }
+            });
+            if (res.ok) {
+                const data = await res.json();
+                setUser(data);
+            }
+            setLoading(false); // Set loading to false after fetching data
+        };
+        const checkAuth = async () => {
+            if (!localStorage.getItem("token")) {
+                router.push("/login");
+            } else {
+                const res = await fetch("/api/auth/check", {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem("token")}`
+                    }
+                });
+                if (!res.ok) {
+                    router.push("/login");
+                } else {
+                    await fetchUserData();
+                }
+            }
+        };
+        checkAuth();
+    }, [id, router]);
+
+    if (loading) {
+        return <div className="min-h-screen flex items-center justify-center text-white bg-gray-900">Loading...</div>;
+    }
+
+    if (!user) {
+        return <div className="min-h-screen flex items-center justify-center text-white bg-gray-900">Loading...</div>;
+    }
+
+    return (
+        <div className='text-white'>
+            <Header />
+            <div className="bg-gray-900 text-white min-h-screen flex flex-col items-center justify-center">
+                <div className="p-4 text-center">
+                    <h1 className="text-4xl mb-4">User Information</h1>
+                    <div className="bg-gray-800 p-6 rounded-lg shadow-lg">
+                        <p className="text-2xl font-semibold mb-2">{user.name}</p>
+                        <p className="text-lg text-gray-400">{user.email}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}

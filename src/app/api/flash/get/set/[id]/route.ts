93096163d9fb16803ca8d942b218d47e0c47eaaa
Aadi Umrani <aadi.p.um@gmail.com>
2024-08-25 00:27:39 -0400

    Opened up another route to unauthenticated users

--- src/app/api/flash/get/set/[id]/route.ts
+++ src/app/api/flash/get/set/[id]/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { connectDB } from '@/app/api/utils/mongodb';
 import FlashSet from '@/app/api/models/FlashSet';
-import jwt, {JwtPayload} from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 
 export async function GET(req: NextRequest) {
     try {
@@ -9,25 +9,29 @@ export async function GET(req: NextRequest) {
 
         const flashSetId = req.nextUrl.pathname.split('/').pop();
         const token = req.headers.get('Authorization')?.split(' ')[1];
+
+        if (!flashSetId) {
+            return NextResponse.json({ error: 'Flash set ID is required' }, { status: 400 });
+        }
+
+        const flashSet = await FlashSet.findById(flashSetId);
+        if (!flashSet) {
+            return NextResponse.json({ error: 'Flash set not found' }, { status: 404 });
+        }
+
         if (!token) {
-            return NextResponse.json({ error: 'Authorization token is required' }, { status: 401 });
+            const { name, set } = flashSet;
+            return NextResponse.json({ name, set });
         }
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET!) as JwtPayload;
         if (!decoded) {
             return NextResponse.json({ error: 'Invalid token' }, { status: 401 });
         }
+
         const userId = decoded.id;
         if (!userId) {
             return NextResponse.json({ error: 'User ID is required' }, { status: 400 });
-        }
-        if (!flashSetId || !userId) {
-            return NextResponse.json({ error: 'Flash set ID and user ID are required' }, { status: 400 });
-        }
-
-        const flashSet = await FlashSet.findById(flashSetId);
-        if (!flashSet) {
-            return NextResponse.json({ error: 'Flash set not found' }, { status: 404 });
         }
 
         return NextResponse.json({ flashSet });

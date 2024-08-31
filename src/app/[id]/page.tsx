79c579acfe4e399e4fb846964bb3409584f2bc23
Aadi Umrani <aadi.p.um@gmail.com>
2024-08-31 19:21:18 -0400

    Made the homepage and flashcard page mobile-compatible

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -66,28 +66,25 @@ export default function FlashSetPage() {
         fetchFlashCards();
     }, [id, router]);
 
-
     return (
         <div className="min-h-screen flex flex-col bg-gray-900 text-white">
             <main className="flex-grow p-4 mt-24">
                 <div className="flex flex-col mb-4 w-full justify-center items-center">
                     <div className="flex flex-row mb-4 w-full justify-center items-center">
-                        <h2 className="text-4xl mr-4 text-center">{flashSetName}</h2>
+                        <h2 className="text-3xl sm:text-4xl mr-4 text-center">{flashSetName}</h2>
                     </div>
-                    <h3 className="text-xl text-gray-400 flex flex-col">
-                        {
-                            userId ? (
-                                (userId !== flashCardUserId) ? (
-                                    <button onClick={() => router.push(`/user/${flashCardUserId}`)} className="hover:underline">
-                                        {author.name}
-                                    </button>
-                                ) : (
-                                    author.name
-                                )
+                    <h3 className="text-lg sm:text-xl text-gray-400 flex flex-col">
+                        {userId ? (
+                            userId !== flashCardUserId ? (
+                                <button onClick={() => router.push(`/user/${flashCardUserId}`)} className="hover:underline">
+                                    {author.name}
+                                </button>
                             ) : (
-                                ''
+                                author.name
                             )
-                        }
+                        ) : (
+                            ''
+                        )}
                     </h3>
                 </div>
                 <div className="flex justify-center items-center">
@@ -98,10 +95,10 @@ export default function FlashSetPage() {
                         {userId === flashCardUserId && (
                             <div className="flex justify-center items-center">
                                 <button
-                                className="text-white border border-gray-700 px-10 py-2 w-min rounded-md bg-gray-800 hover:bg-gray-700 transition duration-150"
-                                onClick={() => router.push(`/${id}/edit`)}
-                        >
-                        Edit
+                                    className="text-white border border-gray-700 px-6 sm:px-10 py-2 w-min rounded-md bg-gray-800 hover:bg-gray-700 transition duration-150"
+                                    onClick={() => router.push(`/${id}/edit`)}
+                                >
+                                    Edit
                                 </button>
                             </div>
                         )}
@@ -109,5 +106,5 @@ export default function FlashSetPage() {
                 </div>
             </main>
         </div>
-);
+    );
 }
--- src/app/widgets/flashcard.tsx
+++ src/app/widgets/flashcard.tsx
@@ -19,18 +19,14 @@ export default function FlashCard({ id, question, answer }: FlashCardProps) {
             className="bg-gray-800 rounded-lg shadow-md transition-all duration-500 cursor-pointer border-2 border-gray-700"
             onClick={toggleAns}
         >
-            <table className="w-full">
-                <tbody>
-                    <tr>
-                        <td className="text-xl font-semibold w-1/2 border-r-2 border-gray-700 p-4">
-                            {question}
-                        </td>
-                        <td className="text-md text-gray-400 text-center font-semibold w-1/2 p-4">
-                            {ansShown ? answer : 'Click to see answer'}
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+            <div className='w-full flex flex-col md:flex-row'>
+                <div className="text-xl font-semibold w-full md:w-1/2 border-b-2 md:border-r-2 md:border-b-0 border-gray-700 p-4">
+                    {question}
+                </div>
+                <div className="text-md text-gray-400 text-center font-semibold w-full md:w-1/2 p-4">
+                    {ansShown ? answer : 'Click to see answer'}
+                </div>
+            </div>
         </div>
     );
 }
